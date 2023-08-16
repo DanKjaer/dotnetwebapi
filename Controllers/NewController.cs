d293b89eda3d0cd956b210140d9eb52e7cc786dd
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-08-16 11:23:14 +0200

    progressCity

---
+++ Controllers/NewController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetwebapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorld : ControllerBase
+{
+    [HttpGet("{id}", Name = "GetWorld")]
+    public String Get([FromRoute] int id)
+    {
+        return $"ID is {id}";
+    }
+}
